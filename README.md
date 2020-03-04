f1fc3d4abe354d5878969700aa09c1c194b8e7a4
Dave Enrico <denrico@northbergen.k12.nj.us>
2020-03-04 08:50:14 -0500

    Add minimal bot functionality to bot.py
    Students need to have a local .env with their own discord token to make
    it work.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project: Discord Bot
+
+This repo contains a minimal working example of a Discord Bot for MP3 project for Intro to Engineering
--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
-# This space for rent
+import os
+from dotenv import load_dotenv
+
+import discord
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.listen()
+async def on_ready():
+    print('Bot loaded and ready!')
+
+@bot.command(name="hi", help="Says hello")
+async def say_hello(ctx):
+    await ctx.send(f"Hi {ctx.author.display_name}")
+
+bot.run(TOKEN)
+
+
