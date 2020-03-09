7681ff4cd086ac765331be012c5f81198c9523fe
Dave Enrico <denrico@northbergen.k12.nj.us>
2020-03-09 13:32:15 -0400

    Try without dotenv

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 import discord
 from discord.ext import commands
 
-load_dotenv()
+# load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
