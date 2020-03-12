c2055a9fc27a4a4199532eb8ab861c4bd61d7080
Dave Enrico <denrico@northbergen.k12.nj.us>
2020-03-12 08:44:22 -0400

    Change token and revert help box changes

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import os
-# from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 import discord
 from discord.ext import commands
 
-# load_dotenv()
+#load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
@@ -15,11 +15,7 @@ async def on_ready():
 
 @bot.command(name="hi", help="Says hello")
 async def say_hello(ctx):
-    box = discord.Embed(title="Help", description="a help box")
-    box.add_field(name="Field_1", value="Hi")
-    box.add_field(name="Field_2", value="Bye")
-    #await ctx.send(f"Hello {ctx.author.display_name}")
-    await ctx.send(embed=box)
+    await ctx.send(f"Hello {ctx.author.display_name}")
 
 bot.run(TOKEN)
 
