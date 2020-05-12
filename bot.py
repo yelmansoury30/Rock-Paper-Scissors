b8e6fc5f0c63a0142eb92d4cfd97e51030c22dea
Youssef Elmansoury <yelmansoury11@gmail.com>
2020-05-12 23:33:00 +0000

    Im mad

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 import discord
 from discord.ext import commands
 
-class Games(command.cog):
+class Games():
     def __init__(self, bot):
         self.bot = bot
 
@@ -34,7 +34,84 @@ class RPS:
             self.choice == RPS.Scissors
         else:
             raise
-client.run('DISCORD_TOKEN')
 
+from random import randint
+
+player = input('rock(r), paper(p), scissors(s)')
+
+if player == 'r':
+    print('✊')
+
+elif player == 'p':
+    print('✋')
+
+elif player == 's':
+    print('✂️')
+
+else:
+    print('By the way we are playing Rock Paper Scissors you imbecile!')
+
+tool = randint(1,3)
+
+if tool == 1:
+    bot = 'r'
+    print('✊')
+
+elif tool == 2:
+    bot = 'p'
+    print('✋')
+
+elif tool == 3:
+    bot = 's'
+    print('✂️')
+
+if player == bot:
+    print('It looks like a tie folks!')
+
+elif player == 'p' and bot == 'r':
+    print('Human Wins!')
+
+elif player == 'r' and bot == 's':
+    print('Human Wins!')
+
+elif player == 's' and bot == 'p':
+    print('Human Wins!')
+
+elif player == 'r' and bot == 'p':
+    print('Bot Wins!')
+
+elif player == 's' and bot == 'r':
+    print('Bot Wins!')
+
+elif player == 's' and bot == 's':
+    print('Bot Wins!')
+
+else:
+    print('Artificial intelligence is not so intelligent after all.')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('{RockPaperScissorsBot} is awake'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!rps'):
+        await message.channel.send('Choose your weapon!')
+
+    if message.content.startswith('r'):
+        await message.channel.send(tool)
+
+    if message.content.startswith('p'):
+        await message.channel.send(tool)
+
+    if message.content.startswith('s️'):
+        await message.channel.send(tool)
+
+client.run('DISCORD_TOKEN')
 
 
