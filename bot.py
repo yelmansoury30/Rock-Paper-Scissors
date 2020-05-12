9d50dcaf8d52b2a0c4003a8fb19f9ae0603a8564
Youssef Elmansoury <yelmansoury11@gmail.com>
2020-05-12 01:45:24 +0000

    Mr. Enrico fixed my code

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import os
 #from dotenv import load_dotenv
 
-import discord
+
 from discord.ext import commands
 
 #load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv(DISCORD_TOKEN)
 
 bot = commands.Bot(command_prefix='!')
 
@@ -15,8 +15,72 @@ async def on_ready():
 
 @bot.command(name="hi", help="Says hello")
 async def say_hello(ctx):
-    await ctx.send(f"Hello {ctx.author.display_name}")
+    await ctx.send(f"Hello {ctx.Yelmansoury}")
 
 bot.run(TOKEN)
+
+
+
+"""
+from random import randint
+
+player = input('rock(r), paper(p), scissors(s)')
+
+if player == 'r':
+    print('✊')
+
+elif player == 'p':
+    print('✋')
+
+elif player == 's':
+    print('✂️')
+
+else:
+    print('By the way we are playing Rock Paper Scissors you imbecile!')
+
+tool = randint(1,3)
+
+if tool == 1:
+    bot = 'r'
+    print('✊')
+
+elif tool == 2:
+    bot = 'p'
+    print('✋')
+
+elif tool == 3:
+    bot = 's'
+    print('✂️')
+
+if player == bot:
+    print('It looks like a tie folks!')
+
+elif player == 'p' and bot == 'r':
+    print('Human Wins!')
+
+elif player == 'r' and bot == 's':
+    print('Human Wins!')
+
+elif player == 's' and bot == 'p':
+    print('Human Wins!')
+
+elif player == 'r' and bot == 'p':
+    print('Bot Wins!')
+
+elif player == 's' and bot == 'r':
+    print('Bot Wins!')
+
+elif player == 's' and bot == 's':
+    print('Bot Wins!')
+
+else:
+    print('Artificial intelligence is not so intelligent after all.')
+"""
+
+
+
+
+
+
 
 
